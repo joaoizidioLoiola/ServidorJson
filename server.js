fa69d11a7f3d80c82076bf95eab8ff1ab3991e4c
joaoizidioLoiola <joaoizidio287@gmail.com>
2024-06-27 18:38:41 -0300

    removendo api

--- server.js
+++ server.js
@@ -1,25 +1,17 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
-
 const server = jsonServer.create()
-
 const router = jsonServer.router('db.json')
+
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(jsonServer.bodyParser);
 
-server.use((req, res, next) => {
-    if (req.method === 'POST') {
-        req.body.createdAt = Date.now()
-    }
-    next()
-})
 
 
 server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
+    '/*': '/$1',
 }))
 server.use(router)
 server.listen(3000, () => {
